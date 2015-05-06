65e1be881d5cfd881c08453e3142b849d4d26059
gastonprieto <gaston.prieto.utn@gmail.com>
2015-05-06 02:31:02 -0300

    Add configuration

---
+++ gulp_tasks/mocha.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var del = require('del');
+var istanbul = require('gulp-istanbul');
+
+function onFinish () {
+  return gulp.src('test/**/*.test.js')
+    .pipe(mocha({ reporter : 'spec' }))
+    .pipe(istanbul.writeReports())
+    .on('error', process.exit.bind(process, 1));
+}
+
+module.exports = function () {
+  process.env.NODE_ENV = 'test';
+  return del('coverage', function () {
+    gulp.src('lib/**/*.js')
+      .pipe(istanbul())
+      .on('finish', onFinish);
+  });
+};
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var gulp = require('gulp');
+var sequence = require('run-sequence');
+
+var tasks = require('require-dir')('gulp_tasks');
+
+gulp.task('mocha', tasks.mocha);
+gulp.task('jshint', tasks.jshint);
+
+gulp.task('test', function (done) {
+  sequence('jshint', 'mocha', done);
+});
